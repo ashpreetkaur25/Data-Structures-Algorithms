e5c076b51c35d5fd978694c15f247f7dbc3a2506
ashpreetkaur25 <82947364+ashpreetkaur25@users.noreply.github.com>
2023-06-05 11:50:50 +0530

    Create Square Pattern.cpp

---
+++ Patterns/Square Pattern.cpp
@@ -0,0 +1,21 @@
+// 1 2 3 4 5 
+// 2 2 3 4 5 
+// 3 3 3 4 5 
+// 4 4 4 4 5 
+// 5 5 5 5 5
+
+
+#include<iostream>
+using namespace std;
+
+int main () {
+	int n;
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=n;j++){
+			j<=i?cout<<i<<" ":cout<<j<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
